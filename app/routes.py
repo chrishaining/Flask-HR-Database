b1bac85c8e371f1ac0cc8ef77debf5be6596664b
Chris <chrishaining@yahoo.co.uk>
2020-02-27 14:21:22 +0000

    commit before a break

--- app/routes.py
+++ app/routes.py
@@ -16,13 +16,45 @@ def show_employees():
     departments = Department.query.all()
     return render_template('employees.html', title="Employees", employees=employees, departments=departments)
 
-
+# add an employee
 @app.route('/employees', methods=['POST'])
 def add_employee():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
+    department = request.form.get('department')
     newEmployee = Employee(first_name=first_name, last_name=last_name)
     db.session.add(newEmployee)
+    newEmployee.departments.append(department)
+    db.session.commit()
+    return redirect('/employees')
+
+# edit employee. I've decided to focus on the names here. I will use a different method for editing the departments.
+@app.route('/employees/<int:employee_id>/update', methods=['POST'])
+def update_employee(employee_id):
+    new_first_name = request.form.get("new_first_name")
+    new_last_name = request.form.get("new_last_name")
+    employee_id = request.form.get("employee_id")
+    employee = Employee.query.filter_by(id=employee_id).first()
+    employee.first_name = new_first_name
+    employee.last_name = new_last_name
+    db.session.commit()
+    return redirect('/employees')
+
+# add a department to an employee
+@app.route('/employees/<int:employee_id>/add_department', methods=['GET', 'POST'])
+# def add_department_to_employee(employee_id, department_id):
+def add_department_to_employee(employee_id, department):
+# def add_department_to_employee():
+    # departments = Department.query.all()
+    # employee_id = request.form.get("employee_id")
+    employee_id = request.form['employee_id']
+    # department_id = request.form.get("department_id")
+    # department = request.form.get("department")
+    department = request.form["department"]
+
+    employee = Employee.query.filter_by(id=employee_id).first()
+    # department = Department.query.filter_by(id=department_id).first()
+    employee.departments.append(department) 
     db.session.commit()
     return redirect('/employees')
 
