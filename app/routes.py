eb033240d912d5d7916f9508784ee20d7649b780
Chris <chrishaining@yahoo.co.uk>
2020-02-27 12:41:42 +0000

    user can add an employee (first_name and last_name)

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, db
 from app.models import Employee, Department, links 
 from flask import render_template, redirect, request
 from sqlalchemy import update
@@ -9,14 +9,27 @@ def index():
     departments = Department.query.all()
     return render_template('index.html', title="Home", employees=employees, departments=departments)
 
+# employees routes
 @app.route('/employees')
 def show_employees():
     employees = Employee.query.all()
     departments = Department.query.all()
-    return render_template('employees.html', title="Home", employees=employees, departments=departments)
+    return render_template('employees.html', title="Employees", employees=employees, departments=departments)
 
+
+@app.route('/employees', methods=['POST'])
+def add_employee():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    newEmployee = Employee(first_name=first_name, last_name=last_name)
+    db.session.add(newEmployee)
+    db.session.commit()
+    return redirect('/employees')
+
+
+# departments routes
 @app.route('/departments')
 def show_departments():
     employees = Employee.query.all()
     departments = Department.query.all()
-    return render_template('departments.html', title="Home", employees=employees, departments=departments)
+    return render_template('departments.html', title="Departments", employees=employees, departments=departments)
